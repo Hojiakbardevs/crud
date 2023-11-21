3784aa8960a11c8c7ff34c222a65865ad2688eb2
Hojiakbar5119 <Abdulhakimovhojiakbar123@gmail.com>
2023-11-21 14:58:07 +0500

    Crud

---
+++ src/Components/Crud.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Crud = () => {
+  return (
+    <div>
+      <h1>Crud Application</h1>
+      <form action="#">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstname" />
+      </form>
+    </div>
+  );
+};
+
+export default Crud;
